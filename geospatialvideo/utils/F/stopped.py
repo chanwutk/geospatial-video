925d2492dbd3539caf91c29f02fba1b9b5c66557
Andrew Lenz <59719367+andrewlenz@users.noreply.github.com>
2022-05-12 12:10:14 -0700

    Modified utility functions to work with sliding operations

--- geospatialvideo/utils/F/stopped.py
+++ geospatialvideo/utils/F/stopped.py
@@ -1,20 +1,21 @@
 from geospatialvideo.instance import Instance
+from geospatialvideo.frame import Frame
 
 from .dist import dist
 
 # Checks if an instance is stopped.
 # 'tol' parameter allows us to consider an object stopped even if there is slight movement,
 # with default tolerance of 0.5 distance units
+# Input should be the result of a sliding operation.
 
-
-def stopped(i: "Instance", tol: float = 0.5) -> bool:
-    locations = []
+def stopped(i: "Instance", f1: "Frame", f2: "Frame", tol: float = 0.5) -> bool:
+    loc1 = []
+    loc2 = []
+    
     for a in i.annotations:
-        locations.append([a.property["x_loc"], a.property["y_loc"], a.property["z_loc"]])
+        if a.frame == f1:
+            loc1 = [a.property["x_loc"], a.property["y_loc"], a.property["z_loc"]]
+        elif a.frame == f2:
+            loc2 = [a.property["x_loc"], a.property["y_loc"], a.property["z_loc"]]
 
-    check = [False] * len(locations)
-    for ix in range(len(locations) - 1):
-        if dist(locations[ix], locations[ix + 1]) <= 0.5:
-            check[ix], check[ix + 1] = True, True
-
-    return any(check)
+    return dist(loc1, loc2) <= 0.5
