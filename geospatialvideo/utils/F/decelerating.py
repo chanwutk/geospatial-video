37d4cd0b7c2da4ecfa52167f1ef80f5e4f8e13ef
Andrew Lenz <59719367+andrewlenz@users.noreply.github.com>
2022-05-12 12:34:33 -0700

    Extension edits (#9)
    * fixed "property" typo
    
    * Modified utility functions to work with sliding operations
    
    * style: [CI] format
    
    Co-authored-by: Github Actions Bot <geospatialvideo-actions-bot@users.noreply.github.com>

--- geospatialvideo/utils/F/decelerating.py
+++ geospatialvideo/utils/F/decelerating.py
@@ -1,22 +1,24 @@
+from geospatialvideo.frame import Frame
 from geospatialvideo.instance import Instance
 
 from .dist import dist
 
 # Checks if an instance is decelerating.
 # Assumes that frames are isochronous; that is, they are recorded at regular time intervals.
+# Input should be the result of two sliding operations.
 
 
-def decelerating(i: "Instance") -> bool:
-    locations = []
+def decelerating(i: "Instance", f1: "Frame", f2: "Frame", f3: "Frame") -> bool:
+    loc1 = []
+    loc2 = []
+    loc3 = []
+
     for a in i.annotations:
-        locations.append([a.property["x_loc"], a.property["y_loc"], a.property["z_loc"]])
+        if a.frame == f1:
+            loc1 = [a.property["x_loc"], a.property["y_loc"], a.property["z_loc"]]
+        elif a.frame == f2:
+            loc2 = [a.property["x_loc"], a.property["y_loc"], a.property["z_loc"]]
+        elif a.frame == f3:
+            loc3 = [a.property["x_loc"], a.property["y_loc"], a.property["z_loc"]]
 
-    distance = 0.0
-    for ix in range(len(locations) - 1):
-        d = dist(locations[ix], locations[ix + 1])
-        if ix > 0:
-            if d - distance > 0:
-                return False
-        distance = d
-
-    return True
+    return dist(loc1, loc2) > dist(loc2, loc3)
--- geospatialvideo/utils/F/move_away.py
+++ geospatialvideo/utils/F/move_away.py
@@ -1,27 +1,25 @@
+from geospatialvideo.frame import Frame
 from geospatialvideo.instance import Instance
 
 from .dist import dist
 
 # Checks for all instances that ever move away from another instance,
-# or have another instance move away from them
+# or have another instance move away from them.
+# Input should be the result of a sliding operation.
 
 
-def move_away(i1: "Instance", i2: "Instance") -> bool:
-    annotations = []
-    for a1 in i1.annotations:
-        for a2 in i2.annotations:
-            if a1.frame == a2.frame:
-                annotations.append([a1, a2])
+def move_away(i1: "Instance", i2: "Instance", f1: "Frame", f2: "Frame") -> bool:
+    i1_loc1 = []
+    i1_loc2 = []
+    i2_loc1 = []
+    i2_loc2 = []
 
-    distances = []
-    for a1, a2 in annotations:
-        x = [a1.property["x_loc"], a1.property["y_loc"], a1.property["z_loc"]]
-        y = [a2.property["x_loc"], a2.property["y_loc"], a2.property["z_loc"]]
-        distances.append(dist(x, y))
+    for a, b in zip(i1.annotations, i2.annotations):
+        if a.frame == f1:
+            i1_loc1 = [a.property["x_loc"], a.property["y_loc"], a.property["z_loc"]]
+            i2_loc1 = [b.property["x_loc"], b.property["y_loc"], b.property["z_loc"]]
+        elif a.frame == f2:
+            i1_loc2 = [a.property["x_loc"], a.property["y_loc"], a.property["z_loc"]]
+            i2_loc2 = [b.property["x_loc"], b.property["y_loc"], b.property["z_loc"]]
 
-    check = [False] * len(distances)
-    for ix in range(len(distances) - 1):
-        if distances[ix] > distances[ix + 1]:
-            check[ix], check[ix + 1] = True, True
-
-    return any(check)
+    return dist(i1_loc1, i2_loc1) < dist(i1_loc2, i2_loc2)
