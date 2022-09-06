a516e6f84a7af261e32b0b888424a180c1ac9eaf
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-09-06 02:22:46 -0700

    add moving

--- geospatialvideo/utils/F/__init__.py
+++ geospatialvideo/utils/F/__init__.py
@@ -2,5 +2,6 @@ from .accelerating import accelerating
 from .decelerating import decelerating
 from .move_away import move_away
 from .stopped import stopped
+from .moving import moving
 
-__all__ = ["stopped", "move_away", "accelerating", "decelerating"]
+__all__ = ["stopped", "move_away", "accelerating", "decelerating", "moving"]
---
+++ geospatialvideo/utils/F/moving.py
@@ -0,0 +1,5 @@
+from geospatialvideo.frame import Frame
+from geospatialvideo.instance import Instance
+
+def moving(i: "Instance", f1: "Frame", f2: "Frame") -> bool:
+    return True
